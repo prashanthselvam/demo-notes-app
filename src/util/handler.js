47d890a89cf0ecd5b77759c4d01a9ff8f61daaee
Prashanth Selvam <prashanth@simondata.com>
2022-05-08 15:56:17 -0400

    Add a get handler

---
+++ src/util/handler.js
@@ -0,0 +1,16 @@
+export default function handler(lambda) {
+  return async function (event, context) {
+    let body, statusCode;
+
+    try {
+      body = await lambda(event, context);
+      statusCode = 200;
+    } catch (e) {
+      console.error(e);
+      body = { error: e.message };
+      statusCode = 500;
+    }
+
+    return { body: JSON.stringify(body), statusCode };
+  };
+}
