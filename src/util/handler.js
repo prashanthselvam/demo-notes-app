2391bda83cb588a05032c54256f8f326dc4050c4
Prashanth Selvam <prashanth@simondata.com>
2022-05-22 15:51:56 -0400

    Enabling cors

--- src/util/handler.js
+++ src/util/handler.js
@@ -11,6 +11,13 @@ export default function handler(lambda) {
       statusCode = 500;
     }
 
-    return { body: JSON.stringify(body), statusCode };
+    return {
+      statusCode,
+      body: JSON.stringify(body),
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Credentials": true,
+      },
+    };
   };
 }
