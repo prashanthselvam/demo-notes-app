70b2ede22a0f221aea739c3dde5a5383400ff905
Prashanth Selvam <prashanth@simondata.com>
2022-05-22 14:16:34 -0400

    Checkpoint

--- stacks/index.js
+++ stacks/index.js
@@ -1,17 +1,23 @@
 import StorageStack from "./StorageStack";
 import ApiStack from "./ApiStack";
+import AuthStack from "./AuthStack";
 
 export default function main(app) {
   // Set default runtime for all functions
   app.setDefaultFunctionProps({
-    runtime: "nodejs14.x"
+    runtime: "nodejs14.x",
   });
 
   const storageStack = new StorageStack(app, "storage");
 
-  new ApiStack(app, "api", {
+  const apiStack = new ApiStack(app, "api", {
     table: storageStack.table,
-  })
+  });
+
+  new AuthStack(app, "auth", {
+    api: apiStack.api,
+    bucket: storageStack.bucket,
+  });
 
   // Add more stacks
 }
