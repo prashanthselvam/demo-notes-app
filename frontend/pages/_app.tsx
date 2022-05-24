d7fb952979198f0856ac11da7ba96b845d8c6a8c
Prashanth Selvam <prashanth@simondata.com>
2022-05-23 23:47:02 -0400

    Add in AWS amplify

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,7 +1,33 @@
 import "../styles/globals.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { Amplify } from "aws-amplify";
+import config from "../lib/config";
 import type { AppProps } from "next/app";
 import Layout from "../components/Layout";
+
+Amplify.configure({
+  Auth: {
+    mandatorySignIn: true,
+    region: config.cognito.REGION,
+    userPoolId: config.cognito.USER_POOL_ID,
+    identityPoolId: config.cognito.IDENTITY_POOL_ID,
+    userPoolWebClientId: config.cognito.APP_CLIENT_ID,
+  },
+  Storage: {
+    region: config.s3.REGION,
+    bucket: config.s3.BUCKET,
+    identityPoolId: config.cognito.IDENTITY_POOL_ID,
+  },
+  API: {
+    endpoints: [
+      {
+        name: "notes",
+        endpoint: config.apiGateway.URL,
+        region: config.apiGateway.REGION,
+      },
+    ],
+  },
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
